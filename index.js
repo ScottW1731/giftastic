5cff2c40f05074e8a5b56bed82d6924a109a14fb
Scott Wagner <af17317596@gmail.com>
2019-02-15 16:19:33 -0600

    for loop & .append

--- index.js
+++ index.js
@@ -7,19 +7,42 @@ $(document).ready(function(){
         console.log(animal);
 
         $.ajax({
-            url: "api.giphy.com/v1/gifs/search?api_key=X97IrbPxwDVu8KnVEQ4ybE2JNhwOkwU0&q=" + animal,
-            dataType: "jsonp"
-        }).done(function(data){
-            console.log(data);
+            url: "http://api.giphy.com/v1/gifs/search?api_key=X97IrbPxwDVu8KnVEQ4ybE2JNhwOkwU0&q=" + animal,
+            method: "GET",
+            // dataType: "jsonp"
+            // for loop through array
+        }).then(function(response){
+            var results = response.data;
+            // process array
+            for (var i = 0; i < results.length; i++) {
+                var still = results[i].images.fixed_height_still.url;
+                console.log(results[i]);
+                console.log("We have data");
+                var animalImage = $("<img>");
+                animalImage.attr("src", still);
+                
+                // target gifs div to append
+                $("#gifs").append(animalImage);
+            
+            }
+            // type/images/fixedheight/url
+            
+            // format data
+            
+            
+            // fix append so that only one tpye of image shows
+            // dynamically create button
+            // functions just like other buttons
+            
         });
 
-        // $.ajax("api.giphy.com/v1/gifs/search?api_key=X97IrbPxwDVu8KnVEQ4ybE2JNhwOkwU0&q=" + animal, function(data){
-        //     console.log(data)
-        // });
+        
     });
 });
 
 // X97IrbPxwDVu8KnVEQ4ybE2JNhwOkwU0    api-key
 // api.giphy.com      endpoint
 // next
-// parse json then append to page
+// parse json 
+// 
+// then append to page
