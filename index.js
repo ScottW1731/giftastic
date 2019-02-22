1d7f314830ad81cdf2737241e608ef4c46ae2a63
Scott Wagner <af17317596@gmail.com>
2019-02-21 18:34:55 -0600

    still tring to get pictures to work

--- index.js
+++ index.js
@@ -1,111 +1,114 @@
 // event handler
-$(document).ready(function () {
-    const animaldiv = $("#animalsbuttons");
-    $(".animal").click(function () {
-        var gifs = $("#gifs");
-        let animal = $(this).data('name');
+$(document).ready(function() {
+  const animaldiv = $("#animalsbuttons");
+  $(".animal").click(function() {
+    var gifs = $("#gifs");
+    let animal = $(this).data("name");
 
-        $.ajax({
-            url: "http://api.giphy.com/v1/gifs/search?api_key=X97IrbPxwDVu8KnVEQ4ybE2JNhwOkwU0&q=" + animal,
-            method: "GET",
+    $.ajax({
+      url:
+        "http://api.giphy.com/v1/gifs/search?api_key=X97IrbPxwDVu8KnVEQ4ybE2JNhwOkwU0&q=" +
+        animal,
+      method: "GET"
 
-            // for loop through array
-        }).then(function (response) {
-            var results = response.data;
-            // process array
-            for (var i = 0; i < results.length; i++) {
-                var still = results[i].images.fixed_height.url;
-                var animate = results[i].images.fixed_height.url;
+      // for loop through array
+    }).then(function(response) {
+      var results = response.data;
+      // process array
+      for (var i = 0; i < results.length; i++) {
+        var still = results[i].images.fixed_height.url;
+        var animate = results[i].images.fixed_height.url;
 
-                var rating = results[i].rating;
-                var p = $("<p>").text("Rating: " + rating);
+        var rating = results[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
 
-                var animalImage = $("<img>");
-                animalImage.attr("src", still);
-                animalImage.attr("src", animate);
+        var animalImage = $("<img>");
+        animalImage.attr("src", still);
+        animalImage.attr("src", animate);
 
-                // target gifs div to append
-                gifs.append(animalImage);
-                gifs.append(p);
-                // TODO: attempt to clear previous image/s displayed
-                // it emptied on loading?
-
-            }
-            // type/images/fixedheight/url
-            // format data
-
-        });
-        // dynamically create button
-        // functions just like other buttons
-        // target div id animalsbuttons
-        // on click to build the button
-        // populate to add it in with other animal buttons
-        console.log("I am working1")
-        $("#gifs").empty();
+        // target gifs div to append
+        gifs.append(animalImage);
+        gifs.append(p);
+        // TODO: attempt to clear previous image/s displayed
+        // it emptied on loading?
+      }
+      // type/images/fixedheight/url
+      // format data
     });
+    // dynamically create button
+    // functions just like other buttons
+    // target div id animalsbuttons
+    // on click to build the button
+    // populate to add it in with other animal buttons
+    // console.log("I am working1");
+    $("#gifs").empty();
+  });
 
-    $(document).on("click", "#submitButton", function () {
-        event.preventDefault()
-        console.log("I am working")
-        let gifs = $("#gifs");
-        
-        var newAnimal = $("#gif-input").val().trim();
+  $(document).on("click", "#submitButton", function() {
+    event.preventDefault();
+    console.log("I am working");
+    let gifs = $("#gifs");
 
-        console.log(newAnimal)
-        var newButton = $("<button/>").addClass("btn btn-info animal").attr('data-name', newAnimal).html(newAnimal).css({ 'margin': '5px' });
+    var newAnimal = $("#gif-input")
+      .val()
+      .trim();
 
-        console.log("newButton", newButton)
+    console.log(newAnimal);
+    var newButton = $("<button/>")
+      .addClass("btn btn-info animal")
+      .attr("data-name", newAnimal)
+      .html(newAnimal)
+      .css({ margin: "5px" });
 
-        // append to page
-        animaldiv.append(newButton);
-        console.log(newAnimal);
-        console.log(animaldiv);
+    console.log("newButton", newButton);
 
-        $.ajax({
-            url: "http://api.giphy.com/v1/gifs/search?api_key=X97IrbPxwDVu8KnVEQ4ybE2JNhwOkwU0&q=" + newAnimal,
-            method: "GET",
-            // dataType: "jsonp"
-            // for loop through array
-        }).then(function (response) {
-            var results = response.data;
-            console.log(response);
-            // process array
-            for (var i = 0; i < results.length; i++) {
-                var results = response.data;
-                var still = results[i].images.fixed_height_still.url;
-                var animate = results[i].images.fixed_height.url;
-                
-                
-                var rating = results[i].rating;
-                var p = $("<p>").text("Rating: " + rating);
+    // append to page
+    animaldiv.append(newButton);
+    console.log(newAnimal);
+    console.log(animaldiv);
 
-                var animalImage = $("<img>");
-                animalImage.attr("src", still);
-                animalImage.attr("src", animate);
+    $.ajax({
+      url:
+        "http://api.giphy.com/v1/gifs/search?api_key=X97IrbPxwDVu8KnVEQ4ybE2JNhwOkwU0&q=" +
+        newAnimal,
+      method: "GET"
+      // dataType: "jsonp"
+      // for loop through array
+    }).then(function(response) {
+      var results = response.data;
+      console.log(response);
+      // process array
+      for (var i = 0; i < results.length; i++) {
+        var results = response.data;
+        var still = results[i].images.fixed_height_still.url;
+        var animate = results[i].images.fixed_height.url;
 
-                // target gifs div to append
-                gifs.append(animalImage);
-                gifs.append(p);
-                // attempt to clear previous image/s displayed
-                // it emptied on loading?
-            }
-        });
-        $("#gifs").empty();
+        var rating = results[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+
+        var animalImage = $("<img>");
+        animalImage.attr("src", still);
+        animalImage.attr("src", animate);
+
+        // target gifs div to append
+        gifs.append(animalImage);
+        gifs.append(p);
+        // attempt to clear previous image/s displayed
+        // it emptied on loading?
+      }
     });
+    $("#gifs").empty();
+  });
 
-    $("#gifs").on("click", function(){
-        console.log("something")
-    });
-
-
+  $("#gifs").on("click", function() {
+    console.log("something");
+  });
 });
 // need on click for images
-
-
 
 // X97IrbPxwDVu8KnVEQ4ybE2JNhwOkwU0    api-key
 // api.giphy.com      endpoint
 // next
-// parse json 
-// 
+// parse json
+//
 // then append to page
